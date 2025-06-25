353f95b40cf92e42532fc28c002c0fc2d839476e
Esihackman <mabelabenahackman602@gmail.com>
2025-06-25 03:03:59 +0000

    worked on the detail note page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { NotesDashboardComponent } from './pages/notes-dashboard/notes-dashboard.component';
 import { CreateNoteComponent } from './pages/create-note/create-note.component';
+import { NoteDetailComponent } from './pages/note-detail/note-detail.component';
 export const routes: Routes = [
  
   { path: '', component: LandingPageComponent },
@@ -11,4 +12,5 @@ export const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'notes', component: NotesDashboardComponent },
   {path: 'create-note', component: CreateNoteComponent},
+  { path: 'notes/:id', component: NoteDetailComponent },
 ];
---
+++ src/app/pages/note-detail/note-detail.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NoteService } from '../../services/note.service';
+import { Note } from '../../models/note.model';
+
+@Component({
+  selector: 'app-note-detail',
+  standalone: true,
+  imports: [CommonModule,RouterModule],
+  templateUrl: './note-detail.component.html',
+  styleUrls: ['./note-detail.component.scss']
+})
+export class NoteDetailComponent implements OnInit {
+  note: Note | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private noteService: NoteService
+  ) {}
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.note = this.noteService.getNoteById(id);
+    }
+  }
+
+  deleteNote() {
+    if (this.note && confirm('Are you sure you want to delete this note?')) {
+      this.noteService.delete(this.note.id);
+      this.router.navigate(['/notes']);
+    }
+  }
+
+  goBack() {
+    this.router.navigate(['/notes']);
+  }
+}
